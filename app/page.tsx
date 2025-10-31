dcb6466388891e292026a4bd31e3897f8a040f1a
Sreevarsh Mahesh Gandhi <sreevarshmaheshgandhi@Sreevarshs-MacBook-Pro.local>
2025-11-01 04:47:32 +0530

    Add missing WhatIsCodity and WhyCodity components to homepage

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,7 @@
 import Link from 'next/link';
 import HeroSection from '@/components/HeroSection';
+import WhatIsCodity from '@/components/ui/What-is-Codity';
+import WhyCodity from '@/components/ui/WhyCodity';
 import ModernSection from '@/components/ModernSection';
 import CyclesTriageSection from '@/components/CyclesTriageSection';
 import AIFeaturesSection from '@/components/AIFeaturesSection';
@@ -11,6 +13,8 @@ export default function Home() {
   return (
     <>
       <HeroSection />
+      <WhatIsCodity />
+      <WhyCodity />
       <ModernSection />
       <CyclesTriageSection />
       <AIFeaturesSection />
